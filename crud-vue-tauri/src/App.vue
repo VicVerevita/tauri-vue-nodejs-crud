4df8fbcfd908a5929a6d93acdb8d945d0cddf03b
Victor <victor.hincu@ausy.com>
2023-05-19 19:24:09 +0300

    started part 5 of tutorial

--- crud-vue-tauri/src/App.vue
+++ crud-vue-tauri/src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <Modal />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {Modal},
+  data() {
+    return {
+      title: "Hello World!",
+    };
+  },
+
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+  },
+};
 </script>
 
 <style>
@@ -22,5 +33,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
