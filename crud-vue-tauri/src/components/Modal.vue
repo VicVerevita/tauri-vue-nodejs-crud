096c6922c3529313e910f5ee84dd04cef55aee23
vicverevita <2victor.gabriel01@gmail.com>
2023-05-22 13:40:23 +0300

    reach chapter 6

--- crud-vue-tauri/src/components/Modal.vue
+++ crud-vue-tauri/src/components/Modal.vue
@@ -1,54 +1,74 @@
 <template>
-    <div class="backdrop">
-        <div class="modal" :class="{ sale: theme === 'sale'}">
-            <h1>{{ header }}</h1>
-            <p>{{ text }}</p>
-        </div>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <slot>default content (if no slot passed in)</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        props: ["header", "text", "theme"],
+export default {
+  props: ['theme'],
+  methods: {
+    closeModal() {
+      this.$emit('close')
     }
+  }
+}
 </script>
 
 <style>
-    .modal {
-        width: 400px;
-        padding: 20px;
-        margin: 100px auto;
-        background: white;
-        border-radius: 10px;
-    }
+  .modal {
+    width: 400px;
+    padding: 20px;
+    margin: 200px auto;
+    background: white;
+    border-radius: 10px;
+  }
+  .backdrop {
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+  }
+  .modal h1 {
+    color: #03cfb4;
+    border: none;
+    padding: 0;
+  }
+  .modal p {
+    font-style: normal;
+  }
+  .modal .actions {
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color: #333;
+  }
+  .modal .actions a {
+    color: #333;
+    padding: 8px;
+    border: 1px solid #eee;
+    border-radius: 4px;
+    text-decoration: none;
+    margin: 10px;
+  }
 
-    .backdrop {
-        position: fixed;
-        top: 0;
-        width: 100%;
-        height: 100vh;
-        background: rgba(0, 0, 0, 0.5);
-    }
-
-    .modal h1 {
-        color: #03cfb4;
-        border: none;
-        padding: 0;
-    }
-
-    .modal p {
-        font-style: normal;
-    }
-
-    .modal.sale {
-
-        background: crimson;
-        color: white;
-
-    }
-
-    .modal.sale h1 {
-        color: white;
-    }
-
+  /* sale styles */
+  .modal.sale {
+    background: crimson;
+    color: white;
+  }
+  .modal.sale h1 {
+    color: white;
+  }
+  .modal.sale .actions {
+    color: white;
+  }
+  .modal.sale .actions a {
+    color: white;
+  }
 </style>
---
+++ reaction-timer/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h1>Reaction Timer</h1>
+  <button @click="start">Play</button>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: { },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    }
+  },
+  methods: {
+    start() {
+      this.isPlaying = true;
+      this.delay = 2000 + Math.random() * 5000
+      
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+</style>
