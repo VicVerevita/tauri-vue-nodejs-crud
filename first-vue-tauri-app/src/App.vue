5f45298b9785006c5b5dc2accf73d0d1f3d32298
vicverevita <2victor.gabriel01@gmail.com>
2023-05-23 16:44:03 +0300

    get started with tauri and build a CRUD to-do list app

---
+++ first-vue-tauri-app/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+    <h1>To-Do List</h1>
+    <Input />
+    <Tasks />
+  </div>
+</template>
+
+<script>
+import {Tasks} from './components/Tasks.vue'
+import {Input} from './components/Input.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Tasks,
+    Input
+  }
+}
+</script>
+
+<style></style>
